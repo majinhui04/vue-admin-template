6481bd0ebdf4d5d03a29ad8b68b0711cf45e5ed9
jinhui.m <majinhui@meeruu.com>
2019-07-09 11:08:36 +0800

    feat: add sh

---
+++ build/release.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+set -e
+
+git checkout master
+git merge develop
+
+VERSION=`npx select-version-cli`
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+
+  # build
+  # VERSION=$VERSION npm run dist
+
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
+
+  # publish
+  git push github master
+  git push origin master
+  git checkout develop
+  git rebase master
+  git push github develop
+
+fi
